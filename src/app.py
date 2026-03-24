62d9f51c92ffea06fa7250250f3b2f0fc79d60db
senai_25_26 <heitortakase5@gmail.com>
2026-03-24 13:38:42 -0300

    mobile

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
+from operator import contains
+
 import flet
-from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment
+from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment, Container, Colors, FontWeight
 from datetime import datetime
 
 
@@ -18,31 +20,35 @@ def main(page: flet.Page):
     def verificacao_numero():
         numero = int(input_numero.value)
         if numero % 2 == 0:
-            text.value = f' Numero {numero} Par'
+            text_parimpar.value = f' Numero {numero} Par'
             page.update()
         else:
-            text.value = f' Numero {numero} Impar'
+            text_parimpar.value = f' Numero {numero} Impar'
             page.update()
 
-    def verificar_idade():
-        #data atual
-        date_now = datetime.today()
-        #data inserida
-        data_nasc = int(input_data_nasc.value)
+    def nascimento():
+        ano_nascimento = int(input_nascimento.value)
+        idade = datetime.today().year - ano_nascimento
 
-        resultado_idade = date_now - data_nasc
-        if resultado_idade <= 18:
-            f''
+        if idade >= 18:
+            text_nascimento.value = f"ele é maior de idade, {idade} anos"
+            page.update()
+        else:
+            text_nascimento.value = f"ele é menor de idade, {idade} anos"
+            page.update()
 
 
 
     # Componentes
     text = Text()
+    text_parimpar = Text()
+    text_nascimento = Text()
     edit_text = TextField(label="Nome")
     sobrenome_text = TextField(label="Sobrenome", on_blur=salvar_nome)
-    input_numero = TextField(label="numero", on_blur=verificacao_numero)
-    input_data_nasc = TextField(hint_text="Data de nascimento", on_blur=verificar_idade)
+    input_numero = TextField(label="numero", on_blur=verificacao_numero, hint_text="verifique se é par ou impar")
+    input_nascimento = TextField(hint_text="data de nascimento", on_blur=nascimento)
     btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
+    btn_numero = OutlinedButton("Verificar", on_click=verificacao_numero)
 
 
 
@@ -51,13 +57,59 @@ def main(page: flet.Page):
     page.add(
         Column(
          [
-             edit_text,
-             sobrenome_text,
-             btn_salvar,
-             input_numero,
+            Container(
+                Column(
+                    [
+                        Text( "atividade 1", weight=FontWeight.BOLD,size=24),
+                        edit_text,
+                        sobrenome_text,
+                        btn_salvar,
+                    ],
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                ),
+
+                bgcolor=Colors.PURPLE_500,
+                padding=15,
+                border_radius=10,
+                width=400,
+
+            ),
+
+             Container(
+                 Column(
+                     [
+                         Text("atividade 2", weight=FontWeight.BOLD, size=24),
+                         input_numero,
+                     ],
+                     horizontal_alignment=CrossAxisAlignment.CENTER,
+                 ),
+
+                 bgcolor=Colors.PURPLE_500,
+                 padding=15,
+                 border_radius=10,
+                 width=400,
+
+             ),
+             Container(
+                 Column(
+                     [
+                         Text("atividade 3", weight=FontWeight.BOLD, size=24),
+                         input_nascimento,
+                     ],
+                     horizontal_alignment=CrossAxisAlignment.CENTER,
+                 ),
+
+                 bgcolor=Colors.PURPLE_500,
+                 padding=15,
+                 border_radius=10,
+                 width=400,
+
+             ),
 
 
-             text
+             text,
+             text_parimpar,
+             text_nascimento
          ],
          width=400,
          horizontal_alignment=CrossAxisAlignment.CENTER
@@ -67,4 +119,4 @@ def main(page: flet.Page):
 
 
 
-flet.app(main)
+flet.run(main)
